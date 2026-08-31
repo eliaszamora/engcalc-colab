f8853288fc46fd9f4d15ac52f7beeb8d3b531d9c
eliaszamora <37598447+eliaszamora@users.noreply.github.com>
2026-08-30 20:10:20 -0400

    test: require EngCalc 0.9.1 package metadata

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -17,12 +17,12 @@ def test_runtime_dependencies_do_not_manage_ipython_in_notebook_hosts():
     assert "ipython" not in _dependency_names()
 
 
-def test_pyproject_version_is_0_9_0():
-    assert _project_metadata()["version"] == "0.9.0"
+def test_pyproject_version_is_0_9_1():
+    assert _project_metadata()["version"] == "0.9.1"
 
 
-def test_runtime_version_is_0_9_0():
-    assert engcalc_colab.__version__ == "0.9.0"
+def test_runtime_version_is_0_9_1():
+    assert engcalc_colab.__version__ == "0.9.1"
 
 
 def test_pint_is_a_runtime_dependency():
